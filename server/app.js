56b634e6fc56f11131d7396e68e532a5a5245b70
dengyulong <dengyulong@oa.gf.com.cn>
2015-10-31 22:26:23 +0800

    update

--- server/app.js
+++ server/app.js
@@ -13,22 +13,20 @@ var config = require('./config/environment');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function (err) {
-        console.error('MongoDB connection error: ' + err);
-        process.exit(-1);
-    }
+mongoose.connection.on('error', function(err) {
+	console.error('MongoDB connection error: ' + err);
+	process.exit(-1);
+	}
 );
 // Populate DB with sample data
-if (config.seedDB) {
-    require('./config/seed');
-}
+if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
-    serveClient: config.env !== 'production',
-    path: '/socket.io-client'
+  serveClient: config.env !== 'production',
+  path: '/socket.io-client'
 });
 require('./config/socketio')(socketio);
 require('./config/express')(app);
@@ -36,7 +34,7 @@ require('./routes')(app);
 
 // Start server
 server.listen(config.port, config.ip, function () {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
 // Expose app
